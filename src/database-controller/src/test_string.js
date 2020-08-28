1b4fa333d7ce281a22723b1046c1da8838e4316b
Zhiyuan He <zhiyuhe@microsoft.com>
2020-08-28 15:22:57 +0800

    fix

---
+++ src/database-controller/src/test_string.js
@@ -0,0 +1,28 @@
+require('module-alias/register');
+require('dotenv').config();
+const fetch = require('node-fetch');
+const AsyncLock = require('async-lock');
+const logger = require('@dbc/common/logger');
+const k8s = require('@dbc/common/k8s');
+const { timePeriod } = require('@dbc/common/util');
+const _ = require('lodash');
+
+async function main() {
+  try{
+    const framework = await k8s.getFramework('7226a3e805c643688b605dc470c24083');
+    const frameworkString = JSON.stringify(framework)
+    const frameworks = [];
+    const n = parseInt(process.argv[2]);
+    console.log(n)
+    for (let i = 0; i<n; i++) {
+      frameworks.push(_.cloneDeep(frameworkString));
+    }
+    console.log('start waiting...')
+    await timePeriod(1000000000);
+  } catch (err) {
+    logger.error(err)
+  }
+
+}
+
+main()
